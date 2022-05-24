1aa0b97f39249fcc974eb93db0786d695f61e23e
Akash Bhaskaran <91144488+akash-bhaskaran@users.noreply.github.com>
2022-05-24 14:01:50 +0530

    return pod ip address

--- session3/resources/flask_app.py
+++ session3/resources/flask_app.py
@@ -1,3 +1,4 @@
+import socket
 from flask import Flask, render_template
 import os
 
@@ -6,9 +7,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+        return ([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
+
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+            port = int(os.environ.get('PORT', 5000))
+            app.run(debug=True, host='0.0.0.0', port=port)
+~                                                          
